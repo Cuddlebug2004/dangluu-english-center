05d61e9d3141f0f14613525f75b520e8994d64e8
Cuddlebug2004 <Cuddlebug241224@gmail.com>
2026-07-13 18:26:15 +0700

    13/07

--- js/course-tabs.js
+++ js/course-tabs.js
@@ -109,12 +109,11 @@ tabs.forEach((tab) => {
 
     image.src = course.image;
     Object.values(courseData).forEach((course) => {
-    const img = new Image();
-    img.src = course.image;
-});
+      const img = new Image();
+      img.src = course.image;
+    });
   });
-}
-);
+});
 
 // Đặt ở cuối cùng file course-tabs.js, bên ngoài vòng lặp tabs.forEach
 Object.values(courseData).forEach((course) => {
