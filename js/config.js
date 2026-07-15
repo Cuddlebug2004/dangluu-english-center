ab86ab6b3bae05d1f4c9ff32c1ac0d20547c3dd1
Cuddlebug2004 <Cuddlebug241224@gmail.com>
2026-07-15 21:04:16 +0700

    1507

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 /**config.js  */
 const API_URL =
-  "https://script.google.com/macros/s/AKfycbwaxRTNRTFg-yiwAGrVNn_YwrQT5zrOeB1-8t-Ue59Z8xazBumVPtcbZfVbhBawHwUk/exec";
+  "https://script.google.com/macros/s/AKfycbx4rQEzFnDvC2SYUGNNeAV-iAulAh8kFFTDdXVMKxhCfaVRUU6giUxoLY-hVF216F2b/exec";
